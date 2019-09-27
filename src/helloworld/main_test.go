d7149b7041b3d8f5f70510dbc543c071121fb6c3
Pranay Kumar <pranay.kumar@synapse.com>
2019-09-27 11:01:33 +0530

    modified test case

--- src/helloworld/main_test.go
+++ src/helloworld/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	var msg = helloWorld()
-	if msg != "Hello World!" {
+	if msg != "Hello World.. This CI works" {
 		t.Errorf("helloWorld did not work, expected Hello World! but got %s", msg)
 	}
 }
